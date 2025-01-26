e08d6b36ff6ab41496506cf8c809936b95fd49a9
Ievgen Osadchyi <osadchyi15@gmail.comi>
2025-01-27 00:14:14 +0100

    buttons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,6 +3,8 @@ import { RiPhoneFill } from "react-icons/ri";
 import { IoPerson } from "react-icons/io5";
 import { useDispatch } from "react-redux";
 import { setDeletingItem, setIsModalStatus } from "../../redux/contacts/slice";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { FaEdit } from "react-icons/fa";
 
 const Contact = ({ name, number, id }) => {
   const dispatch = useDispatch();
@@ -17,22 +19,29 @@ const Contact = ({ name, number, id }) => {
           <RiPhoneFill /> {number}
         </li>
       </ul>
-      <button
-        type="button"
-        onClick={() => {
-          dispatch(
-            setDeletingItem({
-              id,
-              name,
-              number,
-            })
-          );
-          dispatch(setIsModalStatus(true));
-        }}
-        className={css.contactDeletButton}
-      >
-        Delete
-      </button>
+
+      <div className={css.buttons}>
+        <button type="button" className={css.editButtonImage}>
+          <FaEdit className={css.buttonsSize} />
+        </button>
+
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(
+              setDeletingItem({
+                id,
+                name,
+                number,
+              })
+            );
+            dispatch(setIsModalStatus(true));
+          }}
+          className={css.deleteButtonImage}
+        >
+          <RiDeleteBin6Line className={css.buttonsSize} />
+        </button>
+      </div>
     </div>
   );
 };
